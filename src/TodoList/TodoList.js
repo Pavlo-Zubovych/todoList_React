f0a9f00e7b2ba5ed5677d98eb908d93b8151e074
PavloZubovych <zu10@ukr.net>
2022-10-04 23:01:36 +0300

    added complite

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './TodoList.css';
+import classNames from 'classnames';
 
-const TodoList = ({ todos, onDeleteTodo }) => (
+const TodoList = ({ todos, onDeleteTodo, onDoneTodo }) => (
   <ul className='TodoList'>
-    {todos.map(({ id, text }) => (
-      <li key={id} className='TodoList__item'>
-        <p className='TodoList__text'>{text}</p>
+    {todos.map(({ id, text, complited }) => (
+      <li key={id} className='TodoList__item' onClick={() => onDoneTodo(id)}>
+        <p
+          className={classNames('TodoList__text', {
+            TodoList__text__done: complited,
+          })}
+        >
+          {text}
+        </p>
         <button onClick={() => onDeleteTodo(id)}>Видалити</button>
       </li>
     ))}

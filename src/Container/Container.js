39728e8b4ac1bb883f6b8b2a2532d600e4368578
PavloZubovych <zu10@ukr.net>
2022-10-05 21:41:46 +0300

    added new component

---
+++ src/Container/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Container.module.css';
+
+const Container = ({ children }) => {
+  return <div className={styles.Container}>{children}</div>;
+};
+
+export default Container;
